06b21bdc02e1a1482db5a86ab990f07ba920a299
mr258876 <2231327533@qq.com>
2022-06-19 19:56:08 +0800

    Multi language support added.

---
+++ Project-Orgel/lang_en-US.h
@@ -0,0 +1,24 @@
+#ifndef _PROJECT_ORGEL_TEXT_ENG_
+#define _PROJECT_ORGEL_TEXT_ENG_
+#include <Arduino.h>
+
+const PROGMEM char *enUS[] = {
+    "Proj.Orgel",
+    "Now Playing",
+    "Stopped",
+    "Playing",
+    "BPM",
+    "Settings",
+    "Back and Save",
+    "Motor Settings",
+    "Direction",
+    "Gear Teeth",
+    "Current",
+    "About",
+    "Project. Orgel",
+    "@mr_258876",
+    "Ver. 0.2.0",
+    "Language"
+};
+
+#endif
